0dd951cc62a54ae03171db99f49d7804de58bcf6
Mark Sherman <shermanm@emmanuel.edu>
2022-08-23 16:51:56 -0400

    New insights inspired by Atari Quadrascan

--- sketch.js
+++ sketch.js
@@ -1,7 +1,47 @@
+let imageWidth = 640;
+let imageHeight = 480;
+
 function setup () {
-  // put setup code here
+  createCanvas(imageWidth, imageHeight);
+  stroke(255);
+  background(0);
+  translate(width / 2, height / 2);
+  ship();
+  translate(width / -4, height / -4);
+  saucer();
+  translate(0, height / 4);
+  scale(2);
+  saucer();
+  translate(0, height / 8);
+  scale(2);
+  saucer();
 }
 
-function draw () {
-  // put drawing code here
+function ship () {
+  let noseLength = 12;
+  let tailLength = 10;
+  let singleWingSpan = 5;
+  line(noseLength, 0, -tailLength, singleWingSpan);
+  line(-tailLength, singleWingSpan, -5, 0);
+  line(-5, 0, -tailLength, -singleWingSpan);
+  line(-tailLength, -singleWingSpan, noseLength, 0);
+}
+
+function saucer () {
+  let sw = 11;
+  let sh = 3;
+  let bevel = 5;
+  line(-sw, sh, sw, sh);
+  line(sw, sh, sw + bevel, 0);
+  line(sw + bevel, 0, sw, -sh);
+  line(-sw, -sh, sw, -sh);
+  line(-sw, -sh, -sw - bevel, 0);
+  line(-sw - bevel, 0, -sw, sh);
+  // hat
+  let hh = 3;
+  let hw = 5;
+  let hbevel = 2;
+  line(-hw, -sh, -hw + hbevel, -sh - hh);
+  line(-hw + hbevel, -sh - hh, hw - hbevel, -sh - hh);
+  line(hw - hbevel, -sh - hh, hw, -sh);
 }
