98603bb8f5c9d173cbd7bfa7ec3669bbfd3ddf0d
Mark Sherman <shermanm@emmanuel.edu>
2022-12-22 16:39:32 -0500

    start of a parser, again

--- sketch.js
+++ sketch.js
@@ -1,7 +1,32 @@
+function drawShape (def) {
+  for (let arg of arguments) {
+    print(arg);
+  }
+  switch (def.type) {
+    case 'rect':
+      rect(def.x, def.y, def.w, def.h);
+      break;
+  }
+}
+
+let myRect;
+
 function setup () {
-  // put setup code here
+  createCanvas(640, 480);
+  myRect = {
+    type: 'rect',
+    x: 100,
+    y: 50,
+    w: 100,
+    h: 60
+  };
+  stroke(255);
+  noLoop();
 }
 
 function draw () {
-  // put drawing code here
+  background(0);
+  drawShape(myRect);
+  let r = [300, 80, 40, 60];
+  ellipse.apply(undefined, r);
 }
